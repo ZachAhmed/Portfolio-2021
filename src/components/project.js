79af0f1ab5ac346fd627bac1bf9a1ac4db5563bd
Zach <zach.ahmed2@gmail.com>
2020-05-14 02:40:53 -0400

    Added animation package SAL fixed grids added son responsivity

---
+++ src/components/project.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Project({ children }) {
+  return (
+    <div style={{ margin: `10px`, padding: `10px`, backgroundColor:'red'}}>
+    { children }
+    </div>
+  )
+}
+
