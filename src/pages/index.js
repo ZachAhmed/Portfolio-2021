943c0330f97e967f0e7a32b4645c1da2f008ac37
Zach <zach.ahmed2@gmail.com>
2020-05-13 21:35:32 -0400

    text styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Link } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    
-  </Layout>
-)
 
-export default IndexPage
+export default function Index(){
+    return(
+    <Layout> 
+        <div className="h-100 w-screen border-blue border-40 fixed">
+            <div className="grid grid-rows-5 grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-10 h-screen">
+                <container className="col-span-10 row-start-2 ">
+                    <h1 className="text-center text-5xl font-serif text-blue">Zachary Ahmed</h1>
+                    <p className="text-center text-3xl font-sans-serif">The person person</p>
+                     <Link to='/about' className="text-2xl font-serif text-blue row-start-5">About</Link>
+                     <Link to='/about' className="text-2xl font-serif text-blue row-start-5">Contact</Link>
+                </container>
+            </div>
+            <div className="bg-red">
+
+            </div>
+        </div>
+    </Layout> 
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   purge: [],
   theme: {
+
+    fontFamily: {
+      'serif':['Monarcha'],
+      'sans-serif':['Mr-eaves-modern', 'Futura'], 
+    },
+
+    colors: {
+      blue: '#0000FF',
+      red: '#FF0000',
+      orange: '#FFA500',
+    },
+    
     extend: {},
   },
   variants: {},
