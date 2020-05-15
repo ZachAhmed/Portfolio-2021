664581b89cc34000349bcfabfe29f64f4859365d
Zach <zach.ahmed2@gmail.com>
2020-05-14 22:59:19 -0400

    fixed link comp.
    more responsiveness
    
    some animations
    
    changes in animation configs

--- src/components/project.js
+++ src/components/project.js
@@ -1,10 +1,13 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default function Project(props) {
   return (
     <div style={{ margin: `10px`, padding: `10px`, backgroundColor:'red'}}>
-      <h1 className="font-serif text-2xl text-white">{props.pTitle}</h1>
-      <p className="font-sans-serif text-xl mt-3 text-white">{props.pDescription}</p>
+      <Link to='/'>
+        <h1 className="font-serif text-2xl text-white">{props.pTitle}</h1>
+        <p className="font-sans-serif text-xl mt-3 text-white">{props.pDescription}</p>
+      </Link>
     </div>
   )
 }
