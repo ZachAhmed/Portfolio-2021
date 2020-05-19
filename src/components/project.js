73ae5d8134be1ef0c1043b418e9bd09656ad5115
Zach <zach.ahmed2@gmail.com>
2020-05-19 19:39:27 -0400

    .

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,10 @@
 import React from "react"
-import Link from 'gatsby'
 
 export default function Project(props) {
   return (
-    <div class="border-2 border-black rounded-md mt-8 p-2 min-h-full">
-      <h1 class="font-serif text-2xl text-black">{props.pTitle}</h1>
-      <p class="font-sans-serif text-xl mt-4 text-black leading-snug">{props.pDescription}</p>
+    <div className="border-2 border-black rounded-md p-2 min-h-full" data-sal="slide-up" data-sal-delay="0" data-sal-easing="ease" data-sal-duration="100">
+      <h1 className="font-serif text-2xl text-black">{props.pTitle}</h1>
+      <p className="font-sans-serif text-xl mt-4 text-black leading-snug">{props.pDescription}</p>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,40 +3,48 @@ import { Link } from "gatsby"
 import Project from "../components/project"
 import Grid from "../components/grid"
 import Header from "../components/header"
+import sal from "sal.js"
 
+sal();
+
+// const element = document.querySelector('.animated');
+
+// element.addEventListener('sal:out', ({ detail }) => {
+//   console.log('entering', detail.target);
+// });
 
 export default function Index(){
     return(
     <div>
         <Grid>
-            <Header id='top'/>
-            <div class='text-left ml-10 mt-10 col-start-1 col-span-3 row-start-2'>
-                <h1 class='font-serif text-4xl'>Zach Ahmed</h1>
-                <h3 class='font-sans-serif text-xl mt-5'>A person person</h3>
+            <Header id='top'></Header>
+            <div className='text-left ml-10 mt-10 col-start-1 col-span-3 row-start-2'>
+                <h1 className='font-serif text-4xl animated'>Zach Ahmed</h1>
+                <h3 className='font-sans-serif text-xl mt-5'>A person person</h3>
             </div>
         </Grid>
         <Grid>    
-            <Link to='/about' class="row-start-1 col-start-1 col-span-2 ml-10">
-                <Project pTitle='Mypiece Case Study' pDescription='User experience & interface design project for a deal-finding app aimed at university students.'/>
+            <Link to='/about' className="row-start-1 col-start-1 col-span-2 ml-10">
+                <Project pTitle='MyPiece Case Study' pDescription='User experience & interface design project for a deal-finding app aimed at university students.'/>
             </Link>
 
-            <Link to='/about' class="row-start-2 col-start-2 col-span-2 mr-10">
+            <Link to='/about' className="row-start-2 col-start-2 col-span-2 mr-10">
                 <Project pTitle='The Nature Of Things' pDescription='Creative computation project exploring how everyday objects might behave unexpectedly in an alternate reality.'/>
             </Link>
 
-            <Link to='/about' class="row-start-3 col-start-1 col-span-2 ml-10">
+            <Link to='/about' className="row-start-3 col-start-1 col-span-2 ml-10">
                 <Project pTitle='Cumulonimbus' pDescription='An interactive installation designed for display at Dawson college.'/>
             </Link>
 
-            <Link to='/about' class="row-start-4 col-start-2 col-span-2 mr-10">
+            <Link to='/about' className="row-start-4 col-start-2 col-span-2 mr-10">
                 <Project pTitle='Assorted Graphics' pDescription="A collection of illustrations and logos I've designed."/>
             </Link>
 
-            <Link to='/about' class="row-start-5 col-start-1 col-span-2 ml-10">
+            <Link to='/about' className="row-start-5 col-start-1 col-span-2 ml-10">
                 <Project pTitle='Cannabis Laws' pDescription='Data-Visualization project aimed at informing the public about cannabis bylaws in Montreal following legalization.'/>
             </Link>
 
-            <Link to='/#top' class='font-serif text-2xl row-start-6 w-screen text-center my-20 text-blue'>Back to top</Link>
+            <Link to='/#top' className='font-serif text-2xl row-start-6 w-screen text-center my-20 text-blue' data-sal="slide-up" data-sal-delay="0" data-sal-easing="ease" data-sal-duration="100">Back to top</Link>
         </Grid>
     </div>
 
