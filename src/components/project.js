2443b8c86f0edaa2420e63b93fce6e955ce90dcd
Zach <zach.ahmed2@gmail.com>
2020-05-25 13:58:44 -0400

    added image query

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,35 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-export default function Project(props) {
-  return (
+
+
+function Project (props ) {
+  const data = useStaticQuery(graphql`
+    query images {
+        allFile {
+          nodes {
+            childImageSharp {
+              fixed {
+                ...GatsbyImageSharpFixed
+              }
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+  `)
+  console.log(data)
+
+  return(
     <div className="border-4 border-blue rounded-md p-2 min-h-205 md:min-h-full p-3" data-sal="slide-up" data-sal-delay="0" data-sal-easing="ease-in-out-quad" data-sal-duration="200">
       <h1 className="font-serif text-2xl text-blue">{props.pTitle}</h1>
       <p className="font-sans-serif text-xl mt-4 text-blue leading-snug">{props.pDescription}</p>
     </div>
   )
 }
+
+export default Project
 
