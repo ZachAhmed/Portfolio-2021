8d30cbd013d0634fb71489d0c69d8b9aa776256b
Zach <zach.ahmed2@gmail.com>
2020-05-28 13:19:00 -0400

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -27,8 +27,8 @@ function Project (props ) {
     <div className="border-4 border-blue rounded-md p-2 min-h-205 md:min-h-full p-3 col-gap-8" data-sal="slide-up" data-sal-delay="0" data-sal-easing="ease-in-out-quad" data-sal-duration="200">
       <h1 className="font-serif text-2xl text-blue">{props.pTitle}</h1>
       <p className="font-sans-serif text-xl mt-4 text-blue leading-snug">{props.pDescription}</p>
-      <div className='mx-10'>
-        <Img fixed={data.images.childImageSharp.fixed}/>
+      <div className="">
+        <Img className="object-none object-center" fixed={data.images.childImageSharp.fixed}/>
       </div>     
     </div>
   )
