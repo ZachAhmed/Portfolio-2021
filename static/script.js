8c66d4e384e536efc9a945cc37e16bce008767fd
Zach <zach.ahmed2@gmail.com>
2020-07-11 23:47:06 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,14 +2,14 @@ var element = document.querySelector('.animated');
 var t = document.getElementById('name');
 var g = document.getElementsByClassName('generated');
 
-function newFunction_1() {
-    element.addEventListener('sal:in', function () {
-        t.innerHTML = ('');
-    });
-}
 
-function newFunction() {
-    element.addEventListener('sal:out', function () {
-        t.insertAdjacentHTML('beforeend', '<h1 className="md:pl-56"> <a href="/">Zach Ahmed</a> </h1>');
-    });
-}
+element.addEventListener('sal:in', function () {
+    t.innerHTML = ('');
+});
+
+
+
+element.addEventListener('sal:out', function () {
+    t.insertAdjacentHTML('beforeend', '<h1 className="md:pl-56"> <a href="/">Zach Ahmed</a> </h1>');
+});
+
